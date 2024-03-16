323a68ed9960ac46777d5d8b7adcac24abef9663
Mike Bifulco <mbifulco@live.com>
2024-03-16 09:57:01 -0400

    feat: config vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/static';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
@@ -12,4 +13,13 @@ export default defineConfig({
     }),
     react(),
   ],
+
+  // @see hosting with vercel https://docs.astro.build/en/guides/integrations-guide/vercel/
+  output: 'static',
+  adapter: vercel({
+    imageService: true,
+    devImageService: 'squoosh',
+    isr: true,
+    maxDuration: 10, // max # seconds a serverless function can run (to be safe)
+  }),
 });
