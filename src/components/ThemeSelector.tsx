b25537e661c18230867d44c3c2c1fec241408895
Mike Bifulco <mbifulco@live.com>
2024-03-25 07:39:19 -0400

    wip: cleanup

--- src/components/ThemeSelector.tsx
+++ src/components/ThemeSelector.tsx
@@ -5,7 +5,7 @@ import clsx from 'clsx';
 const themes = [
   { name: 'Light', value: 'light', icon: LightIcon },
   { name: 'Dark', value: 'dark', icon: DarkIcon },
-  { name: 'System', value: 'system', icon: SystemIcon },
+  // { name: 'System', value: 'system', icon: SystemIcon },
 ];
 
 function LightIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
