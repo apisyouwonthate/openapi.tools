17253c79e7e1ba24a4d9119db9e98564973500ce
Mike Bifulco <mbifulco@live.com>
2024-04-07 11:28:56 -0400

    fix: table header size

--- src/components/table/DataTable.tsx
+++ src/components/table/DataTable.tsx
@@ -31,7 +31,7 @@ export function DataTable<TData, TValue>({
 
   return (
     <div className="rounded-md border">
-      <Table>
+      <Table className="m-0 p-0">
         <TableHeader>
           {table.getHeaderGroups().map((headerGroup) => (
             <TableRow key={headerGroup.id}>
