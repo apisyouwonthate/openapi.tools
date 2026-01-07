f53690c38674ff21dc511fdd6c2a0580c237d048
Phil Sturgeon <67381+philsturgeon@users.noreply.github.com>
2026-01-07 18:57:22 +0000

    fix: use website icon instead of Website

--- src/components/table/CollectionToolColumns.tsx
+++ src/components/table/CollectionToolColumns.tsx
@@ -1,6 +1,7 @@
 import { type ColumnDef } from '@tanstack/react-table';
 
 import RepoIcon from '../icons/RepoIcon';
+import WebsiteIcon from '../icons/WebsiteIcon';
 import Link from '../Link';
 import type { ToolRowData } from './Columns';
 
@@ -65,7 +66,7 @@ export const CollectionToolColumns: ColumnDef<ToolRowData>[] = [
     cell: ({ row }) => {
       const { tool, category } = row.original;
       return (
-        <div className="flex flex-col space-y-1">
+        <div className="flex flex-row items-center space-x-2">
           {tool?.link && (
             <Link
               href={tool.link}
@@ -73,7 +74,7 @@ export const CollectionToolColumns: ColumnDef<ToolRowData>[] = [
               category={category}
               linkPlacementDescription="collection-tools-table"
             >
-              Website
+              <WebsiteIcon className="inline-block h-4 w-4 fill-slate-800 dark:fill-white" />
             </Link>
           )}
 
